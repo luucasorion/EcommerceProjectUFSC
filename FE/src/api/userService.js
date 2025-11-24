35419ee83db50bab37cc9f40527b6587c0d05259
luucasorion <lucasorionsouzadasilva@gmail.com>
2025-11-24 17:51:53 -0300

    fix: url correta

--- FE/src/api/userService.js
+++ FE/src/api/userService.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5056";
+const API_URL = "https://ecommerceprojectufsc-cfh3hqf0brgthrd9.eastus2-01.azurewebsites.net";
 
 export async function getUser(token) {
     const response = await fetch(API_URL + '/user', {
